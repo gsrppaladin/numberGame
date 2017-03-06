66e7b146aac647077adb71d7d0128e21a949f7f9
Sam Greenhill <samgreenhill@Sams-MBP.home>
2017-03-05 21:24:23 -0500

    finishing the code for the game

--- NumberGame/ViewController.swift
+++ NumberGame/ViewController.swift
@@ -35,6 +35,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var btnRestart: UIButton!
     
     
+    var noOfGuessesMade = 0
+    var totalGuesses = 10
+    
     
     
     
@@ -44,34 +47,94 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        startGame()
     }
 
 
     @IBAction func lowerPressed(_ sender: UIButton) {
-        print("lower pressed")
+       
+        if noOfGuessesMade >= totalGuesses {
+            gameLost()
+            return
+        }
+        higherNumber = lastGuessed
+        lastGuessed = guessNumber(lowerNum: lowerNumber, higherNum: higherNumber)
+        askQuestion()
+        
+        
     }
 
     
     @IBAction func yesPressed(_ sender: UIButton) {
-        print("yes pressed")
+        questionLbl.text = "I Won!!!!"
+        showHideBtns(hidden: true)
     }
 
     
     @IBAction func higherPressed(_ sender: UIButton) {
-        print("higher pressed")
+        
+        if noOfGuessesMade >= totalGuesses {
+            gameLost()
+            return
+        }
+        lowerNumber = lastGuessed
+        lastGuessed = guessNumber(lowerNum: lowerNumber, higherNum: higherNumber)
+        askQuestion()
     }
     
     
     @IBAction func restartPressed(_ sender: UIButton) {
-        print("restart pressed")
+        startGame()
+        showHideBtns(hidden: false)
+    }
+
+    func startGame() {
+        //when you restart game
+        noOfGuessesMade = 0
+        number1 = Int(arc4random_uniform(1000))
+        number2 = Int(arc4random_uniform(1000))
+        
+        //lower number is = if number1 is less then number2, and if it is that case store number1 into lower number, right hand is stored into the lefthand side. if number1 is less then number2 then give me number1 and store it in lowerNumber, otherwise give me number2
+        lowerNumber = number1 < number2 ? number1 : number2
+        
+        higherNumber = number1 > number2 ? number1 : number2
+        
+        lastGuessed = guessNumber(lowerNum: lowerNumber, higherNum: higherNumber)
+        
+        messageLbl.text = "Think of a number between \(lowerNumber) and \(higherNumber)"
+        
+        askQuestion()
+    }
+    
+    func guessNumber(lowerNum: Int, higherNum: Int) -> Int {
+    
+        noOfGuessesMade+=1
+        remainingGuessesLbl.text = "\(totalGuesses - noOfGuessesMade)"
+        
+        return (higherNum - lowerNum) / 2 + lowerNum
+        //example of how this works. 
+        //if lowerNumber = 200 and higherNumber = 500, do (500 - 200) / 2 = 150 + 200 = 350
+        
+        
+    }
+    
+    func askQuestion() {
+        questionLbl.text = "Is your number \(lastGuessed)"
     }
     
     
-    
-    
-    
-    
+    func showHideBtns(hidden: Bool) {
+        btnYes.isHidden = hidden
+        btnLower.isHidden = hidden
+        btnHigher.isHidden = hidden
+        
+    }
+    func gameLost() {
+        questionLbl.text = "You Win"
+        showHideBtns(hidden: true)
+    }
 
+    
+    
 }
 
