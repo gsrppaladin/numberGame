3ce5839c65feb9fc20338df7a6d72f5886120fbb
Sam Greenhill <samgreenhill@Sams-MBP.home>
2017-03-05 20:24:20 -0500

    Creating the UI and all the buttons and labels

--- NumberGame/ViewController.swift
+++ NumberGame/ViewController.swift
@@ -10,16 +10,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var number1 = 0 // holds 1 random number
+    var number2 = 0 // holds second number
+    //which one is higher and which one is lower so we store it in another
+    
+    var lowerNumber = 0
+    var higherNumber = 0
+    //track the last guess the computer made.
+    
+    var lastGuessed = 0
+    //will need number of guesses made and allowed
+    
+    @IBOutlet weak var messageLbl: UILabel!
+    
+    @IBOutlet weak var questionLbl: UILabel!
+    
+    
+    @IBOutlet weak var remainingGuessesLbl: UILabel!
+    
+    
+    @IBOutlet weak var btnLower: UIButton!
+    @IBOutlet weak var btnYes: UIButton!
+    @IBOutlet weak var btnHigher: UIButton!
+    @IBOutlet weak var btnRestart: UIButton!
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    @IBAction func lowerPressed(_ sender: UIButton) {
+        print("lower pressed")
     }
 
+    
+    @IBAction func yesPressed(_ sender: UIButton) {
+        print("yes pressed")
+    }
+
+    
+    @IBAction func higherPressed(_ sender: UIButton) {
+        print("higher pressed")
+    }
+    
+    
+    @IBAction func restartPressed(_ sender: UIButton) {
+        print("restart pressed")
+    }
+    
+    
+    
+    
+    
+    
 
 }
 
